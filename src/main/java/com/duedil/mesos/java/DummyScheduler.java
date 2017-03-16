e27fcd48a9e03024d1fc37c62742a573d3a72bdc
Oli Hall <oliver.hall@duedil.com>
2017-03-16 09:56:54 +0000

    Extend DummyScheduler to create a Task in response to offer

--- src/main/java/com/duedil/mesos/java/DummyScheduler.java
+++ src/main/java/com/duedil/mesos/java/DummyScheduler.java
@@ -1,13 +1,17 @@
 package com.duedil.mesos.java;
 
+import com.google.api.client.util.Lists;
 import com.google.protobuf.ByteString;
 
 import org.apache.mesos.v1.Protos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 
 import java.net.URI;
 import java.util.List;
+import java.util.UUID;
+import java.util.logging.LogManager;
 
 /**
  * Test scheduler to verify basic Scheduler behaviours
@@ -47,6 +51,77 @@ public class DummyScheduler implements Scheduler {
     @Override
     public void resourceOffers(SchedulerDriver driver, List<Protos.Offer> offers) {
         LOG.info("OFFERED RESOURCE");
+
+        double offerCpu = 0.0;
+        double offerMem = 0.0;
+        String offerRole = "";
+        Protos.AgentID agentId = null;
+        List<Protos.OfferID> offerIds = Lists.newArrayList();
+        for (Protos.Offer offer : offers) {
+
+            offerIds.add(offer.getId());
+            agentId = offer.getAgentId();
+
+            for (Protos.Resource resource : offer.getResourcesList()) {
+                offerRole = resource.getRole();
+                if ("cpus".equals(resource.getName())) {
+                    offerCpu = resource.getScalar().getValue();
+                } else if ("mem".equals(resource.getName())) {
+                    offerMem = resource.getScalar().getValue();
+                }
+            }
+
+            LOG.debug(String.format("Received offer for CPUs: %f, Mem: %f, Role: %s", offerCpu, offerMem, offerRole));
+            System.out.println(String.format("Received offer for CPUs: %f, Mem: %f, Role: %s", offerCpu, offerMem, offerRole));
+        }
+
+        Protos.TaskID taskId = Protos.TaskID.newBuilder()
+                .setValue(UUID.randomUUID().toString())
+                .build();
+        Protos.TaskInfo taskInfo = Protos.TaskInfo.newBuilder()
+                .setName("TestyMcTestFace")
+                .setTaskId(taskId)
+                .setAgentId(agentId)
+                .setExecutor(
+                        Protos.ExecutorInfo.newBuilder()
+                                .setExecutorId(
+                                        Protos.ExecutorID.newBuilder()
+                                                .setValue(taskId.getValue())
+                                                .build()
+                                )
+                                .build()
+                ) // TODO a command etc
+                .addResources(
+                        Protos.Resource.newBuilder()
+                                .setName("cpus")
+                                .setType(Protos.Value.Type.SCALAR)
+                                .setRole(offerRole)
+                                .setScalar(
+                                        Protos.Value.Scalar.newBuilder()
+                                                .setValue(offerCpu)
+                                                .build()
+                                )
+                )
+                .addResources(
+                        Protos.Resource.newBuilder()
+                                .setName("mem")
+                                .setType(Protos.Value.Type.SCALAR)
+                                .setRole(offerRole)
+                                .setScalar(
+                                        Protos.Value.Scalar.newBuilder()
+                                                .setValue(offerMem)
+                                                .build()
+                                )
+                )
+                .build();
+
+        List<Protos.TaskInfo> tasks = Lists.newArrayList();
+        tasks.add(taskInfo);
+
+
+        System.out.println(String.format("Launching tasks %s for offer IDs %s", tasks.toString(), offerIds.toString()));
+        LOG.debug(String.format("Launching tasks %s for offer IDs %s", tasks.toString(), offerIds.toString()));
+        driver.launchTasks(offerIds, tasks, null);
     }
 
     @Override
@@ -56,7 +131,12 @@ public class DummyScheduler implements Scheduler {
 
     @Override
     public void statusUpdate(SchedulerDriver driver, Protos.TaskStatus status) {
-        LOG.info("STATUS UPDATE");
+        LOG.info("Task update: " + status.getState().toString());
+        switch (status.getState()) {
+            case TASK_FAILED:
+                LOG.error("Task failed: " + status.getMessage());
+        }
+
     }
 
     @Override
