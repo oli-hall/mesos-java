93dbdeec765151b2ff57b516e238243b1b62074e
Pedro Figueiredo <pedro@duedil.com>
2017-04-05 17:32:12 +0100

    MesosExecutorDriver initialisation

--- src/main/java/com/duedil/mesos/java/executor/MesosExecutorDriver.java
+++ src/main/java/com/duedil/mesos/java/executor/MesosExecutorDriver.java
@@ -1,16 +1,44 @@
-package com.duedil.mesos.java;
+package com.duedil.mesos.java.executor;
 
-import org.apache.mesos.v1.Protos;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import org.apache.mesos.v1.Protos.ExecutorInfo;
+import org.apache.mesos.v1.Protos.FrameworkID;
+import org.apache.mesos.v1.Protos.FrameworkInfo;
+import org.apache.mesos.v1.Protos.Status;
+import org.apache.mesos.v1.executor.Protos.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class MesosExecutorDriver implements ExecutorDriver {
+import static com.google.api.client.util.Preconditions.checkNotNull;
+
+public class MesosExecutorDriver implements ExecutorDriver, ActionableListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MesosExecutorDriver.class);
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+    private final Executor executor;
+    private final FrameworkInfo framework;
+    private final ExecutorInfo executorInfo;
+    private FrameworkID frameworkId;
+    private ExecutorConnection conn;
+
+    public MesosExecutorDriver(Executor executor, FrameworkInfo framework, ExecutorInfo executorInfo) {
+        this.executor = checkNotNull(executor);
+        this.framework = checkNotNull(framework);
+        this.frameworkId = framework.getId();
+        this.executorInfo = checkNotNull(executorInfo);
+        this.conn = null;
+    }
+
     @Override
     public void start() {
-
+        conn = new ExecutorConnection(framework, frameworkId, executorInfo, this);
     }
 
     @Override
     public void stop() {
-
+        // TODO: kill all the things
     }
 
     @Override
@@ -20,21 +48,33 @@ public class MesosExecutorDriver implements ExecutorDriver {
 
     @Override
     public void join() {
-
+        try {
+            conn.join();
+        }
+        catch (InterruptedException e) {
+            LOG.error("Exception thrown waiting for connection join: {}", e.getMessage());
+        }
     }
 
     @Override
     public void run() {
-
+        start();
+        conn.run();
+        join();
     }
 
     @Override
-    public void sendStatusUpdate(Protos.Status status) {
+    public void sendStatusUpdate(Status status) {
 
     }
 
     @Override
     public void sendFrameworkMessage(byte[] data) {
 
+    }
+
+    @Override
+    public void onEvent(Event event) {
+        LOG.debug("Event: {}", event.getMessage());
     }
 }
